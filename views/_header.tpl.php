a3bbeffa868d6f9206c03978710ba0094edc5eaa
Theodore R. Smith <theodore@phpexperts.pro>
2013-09-07 12:19:50 -0500

    [m] Made all of the asset URLs relative to the project.

--- views/_header.tpl.php
+++ views/_header.tpl.php
@@ -17,13 +17,12 @@ if (empty($meta->title)) { $meta->title = 'PHP University'; }
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <!--<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />-->
-		<meta charset="utf-8"/>
+        <meta charset="utf-8"/>
         <title><?php echo $meta->title; ?> | PHP University</title>
-        <link href="/css/default.css" rel="stylesheet" type="text/css" media="all" />
-        <link rel="stylesheet" type="text/css" href="/taxes/individual/main.css" media="all" />                  <!-- CUSTOM -->
-        <!--[if IE 6]>
-        <link href="/css/default_ie6.css" rel="stylesheet" type="text/css" />
+        <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
+	    <link href="css/main.css" rel="stylesheet" type="text/css" media="all" />
+	    <!--[if IE 6]>
+        <link href="css/default_ie6.css" rel="stylesheet" type="text/css" />
         <![endif]-->
         <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <?php
@@ -35,16 +34,16 @@ if (!empty($customCSS)) {
 	}
 }
 ?>
-        <script type="text/javascript" src="/js/prettify.js"></script>                                   <!-- PRETTIFY -->
-        <script type="text/javascript" src="/js/kickstart.js"></script>                                  <!-- KICKSTART -->
+        <script type="text/javascript" src="js/prettify.js"></script>                                   <!-- PRETTIFY -->
+        <script type="text/javascript" src="js/kickstart.js"></script>                                  <!-- KICKSTART -->
         <script src="js/jquery-1.8.3.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="/css/kickstart.css" media="all" />                  <!-- KICKSTART -->
+        <link rel="stylesheet" type="text/css" href="css/kickstart.css" media="all" />                  <!-- KICKSTART -->
         <meta name="description" content="Federal Income Tax Calculator 2012 2013: Fiscal cliff tax calculator"/>
         <meta property="og:image" content="<?php echo $meta->image; ?>"/>
         <meta property="og:title" content="<?php echo $meta->title; ?>"/>
         <meta property="og:type" content="website"/>
         <meta property="og:url" content="http://www.phpu.cc/taxes/individual/"/>
-        <script type="text/javascript" src="/js/external.js"></script>
+        <script type="text/javascript" src="js/external.js"></script>
 <?php if (!empty($customHeader)) { echo $customHeader; } ?>
     </head>
 <body>
